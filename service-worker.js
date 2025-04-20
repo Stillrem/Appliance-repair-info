aeebc6bf347cd47d1cbdb08a68953cb7efb18e97
Stillrem <106412413+Stillrem@users.noreply.github.com>
2025-04-20 16:15:00 -0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('pwa-cache').then(cache => cache.addAll(['index.html', 'manifest.json'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
-});
